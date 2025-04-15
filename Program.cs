e5d220618f4aea74ff402706b567b585b471efca
AJ Cunha <argenisjc@hotmail.com>
2025-04-15 17:19:27 -0500

    Commit 2

--- Program.cs
+++ Program.cs
@@ -3,7 +3,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("INFORMATION INPUT");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.Write("What is your age? ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+
+
+
+        Person Person1 = new Person(firstName, lastName, age, color);
+
+        Person1.DisplayResults();
+
+        Console.Read();
+    }
+    public class Person
+    {
+        public string firstName, lastName, color;
+        public int age;
+
+        public Person(string firstName, string lastName, int age, string color)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.color = color;
+
+        }
+
+        public void DisplayResults()
+        {
+            Console.WriteLine("Hello, " + firstName + " " + lastName + " You are " + age + " Years old and your favorite color is " + color + ".");
+        }
     }
 }
 
